53e5fdce64a07e46be9860504ed321041c447ad5
peKe267 <c0116267db@edu.teu.ac.jp>
2018-10-25 17:00:44 +0900

    Object-orientation

--- 6/BlockBreaking.cpp
+++ 6/BlockBreaking.cpp
@@ -23,6 +23,59 @@ public:
 	}
 };
 
+class Player {
+public:
+	int iPx, iPy, iWidth, iHeight, iSpeed;
+
+	Player() {
+		iPx = 320;
+		iPy = 400;
+		iWidth = 60;
+		iHeight = 20;
+		iSpeed = 10;
+	}
+
+	//作成
+	void View() {
+		DrawBox(iPx, iPy, iPx + iWidth, iPy + iHeight,
+			GetColor(255, 255, 255), TRUE);
+	}
+
+	void Move() {
+		if (CheckHitKey(KEY_INPUT_RIGHT) && iPx + 60 < 640) {
+			iPx += iSpeed;
+		}
+		if (CheckHitKey(KEY_INPUT_LEFT) && iPx > 0) {
+			iPx -= iSpeed;
+		}
+	}
+};
+
+class Ball {
+public:
+	int iBallX, iBallY, iBallR ;
+	int iVecX, iVecY, iSpeed;
+
+	Ball() {
+		iBallX = 320;
+		iBallY = 240;
+		iBallR = 10;
+		iVecX = 0;
+		iVecY = 0;
+		iSpeed = 5;
+	}
+
+	//作成
+	void View() {
+		DrawCircle(iBallX, iBallY, iBallR, GetColor(255, 255, 255), TRUE);
+	}
+
+	void Move() {
+		iBallX += iSpeed * iVecX;
+		iBallY += iSpeed * iVecY;
+	}
+};
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow) {
 	//ウィンドウモード
@@ -32,15 +85,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	//ダブルバッファ(準備)
 	SetDrawScreen(DX_SCREEN_BACK);
 
-	int iBallX = 320, iBallY = 240, iBallR = 10;
-	int iVecX = 0, iVecY = 0, iLevel = 5;
-	int iPx = 320, iPy = 400;
-
 	//ポインタ（https://qiita.com/hmito/items/44925fca9fca74e78f61）
 	Block* pBl[20];
 	for (int i = 0; i < 20; i++) {
 		pBl[i] = new Block(100 + 110 * (i % 4), 40 + 40 * (i / 4));
 	}
+	Player* pPl = new Player();
+	Ball* pB = new Ball();
 
 	while (ProcessMessage() != -1) {
 		//ダブルバッファ(表示、削除)
@@ -48,60 +99,59 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		ClearDrawScreen();
 
 		//球体の表示
-		DrawCircle(iBallX, iBallY, iBallR, GetColor(255, 255, 255), TRUE);
-		//バーの表
-		DrawBox(iPx, iPy, iPx + 60, iPy + 20,
-			GetColor(255, 255, 255), TRUE);
+		pB->View();
+		//バー表示
+		pPl->View();
 
 		for (int i = 0; i < 20; i++)
 		{
 			pBl[i]->View();
 		}
 
-		//球体の動作
-		if (iVecX != 0 && iVecX != 0) {
-			if (iBallX > 640)iVecX = -1;
-			if (iBallX < 0)iVecX = 1;
-			if (iBallY < 0)iVecY = 1;
-			if (iBallX > iPx && iBallX + 10 < iPx + 60 && iBallY + 10 > iPy)iVecY = -1;
-			if (iBallY > 480) {
-				iBallX = 320;
-				iBallY = 240;
-				iVecX = 0;
-				iVecY = 0;
+		//球体の当たり判定
+		if (pB->iVecX != 0 && pB->iVecX != 0) {
+			if (pB->iBallX > 640)pB->iVecX = -1;
+			if (pB->iBallX < 0)pB->iVecX = 1;
+			if (pB->iBallY < 0)pB->iVecY = 1;
+			if (pB->iBallX > pPl->iPx && pB->iBallX + 10 < pPl->iPx + 60 &&
+				pB->iBallY + 10 > pPl->iPy)pB->iVecY = -1;
+			if (pB->iBallY > 480) {
+				pB->iBallX = 320;
+				pB->iBallY = 240;
+				pB->iVecX = 0;
+				pB->iVecY = 0;
 			}
-			//当たり判定
 			for (int i = 0; i < 20; i++) {
 				if (pBl[i]->bExist) {
-					if (iBallX > pBl[i]->iBlockX && iBallX < pBl[i]->iBlockX + pBl[i]->iWidth &&
-						iBallY + iBallR > pBl[i]->iBlockY && iBallY + iBallR < pBl[i]->iBlockY + pBl[i]->iHeight) {//上
+					if (pB->iBallX > pBl[i]->iBlockX && pB->iBallX < pBl[i]->iBlockX + pBl[i]->iWidth &&
+						pB->iBallY + pB->iBallR > pBl[i]->iBlockY && pB->iBallY + pB->iBallR < pBl[i]->iBlockY + pBl[i]->iHeight) {//上
 						pBl[i]->bExist = false;
-						iVecY *= -1;
+						pB->iVecY *= -1;
 					}
-					if (iBallX > pBl[i]->iBlockX && iBallX < pBl[i]->iBlockX + pBl[i]->iWidth &&
-						iBallY - iBallR > pBl[i]->iBlockY && iBallY - iBallR < pBl[i]->iBlockY + pBl[i]->iHeight) {//下
+					if (pB->iBallX > pBl[i]->iBlockX && pB->iBallX < pBl[i]->iBlockX + pBl[i]->iWidth &&
+						pB->iBallY - pB->iBallR > pBl[i]->iBlockY && pB->iBallY - pB->iBallR < pBl[i]->iBlockY + pBl[i]->iHeight) {//下
 						pBl[i]->bExist = false;
-						iVecY *= -1;
+						pB->iVecY *= -1;
 					}
-					if (iBallX + iBallR > pBl[i]->iBlockX && iBallX + iBallR < pBl[i]->iBlockX + pBl[i]->iWidth &&
-						iBallY > pBl[i]->iBlockY && iBallY < pBl[i]->iBlockY + pBl[i]->iHeight) {//左
+					if (pB->iBallX + pB->iBallR > pBl[i]->iBlockX && pB->iBallX + pB->iBallR < pBl[i]->iBlockX + pBl[i]->iWidth &&
+						pB->iBallY > pBl[i]->iBlockY && pB->iBallY < pBl[i]->iBlockY + pBl[i]->iHeight) {//左
 						pBl[i]->bExist = false;
-						iVecX *= -1;
+						pB->iVecX *= -1;
 					}
-					if (iBallX - iBallR > pBl[i]->iBlockX && iBallX - iBallR < pBl[i]->iBlockX + pBl[i]->iWidth &&
-						iBallY > pBl[i]->iBlockY && iBallY < pBl[i]->iBlockY + pBl[i]->iHeight) {//右
+					if (pB->iBallX - pB->iBallR > pBl[i]->iBlockX && pB->iBallX - pB->iBallR < pBl[i]->iBlockX + pBl[i]->iWidth &&
+						pB->iBallY > pBl[i]->iBlockY && pB->iBallY < pBl[i]->iBlockY + pBl[i]->iHeight) {//右
 						pBl[i]->bExist = false;
-						iVecX *= -1;
+						pB->iVecX *= -1;
 					}
 				}
 			}
 		}
 		//スペースキーでスタート
 		else {
-			DrawFormatString(260, 160, GetColor(255, 255, 255), "PUSH SPACE");
+			DrawFormatString(260, 160, GetColor(128, 128, 128), "PUSH SPACE");
 			if (CheckHitKey(KEY_INPUT_SPACE)) {
-				iVecX = 1;
-				iVecY = 1;
+				pB->iVecX = 1;
+				pB->iVecY = 1;
 			}
 		}
 		//ブロックがなくなればゲームクリア
@@ -109,25 +159,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		{
 			if (pBl[i]->bExist)break;
 			if (i == 19) {
-				DrawFormatString(260, 120, GetColor(255, 255, 255), "GameClear!");
-				iBallX = 320;
-				iBallY = 240;
-				iVecX = 0;
-				iVecY = 0;
+				DrawFormatString(260, 120, GetColor(128, 128, 128), "GAME CLEAR!");
+				pB->iBallX = 320;
+				pB->iBallY = 240;
+				pB->iVecX = 0;
+				pB->iVecY = 0;
 			}
 		}
 
 		//球体の移動
-		iBallX += iLevel * iVecX;
-		iBallY += iLevel * iVecY;
+		pB->Move();
 
 		//バーの移動
-		if (CheckHitKey(KEY_INPUT_RIGHT) == 1 && iPx+60 < 640) {
-			iPx += 10;
-		}
-		if (CheckHitKey(KEY_INPUT_LEFT) == 1 && iPx > 0) {
-			iPx -= 10;
-		}
+		pPl->Move();
 
 		//escapeキーによる終了
 		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)break;
@@ -138,7 +182,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	{
 		delete pBl[i];
 	}
+	delete pPl;
+	delete pB;
 
+	// ＤＸライブラリ終了
 	DxLib_End();
 	return 0;
 }
